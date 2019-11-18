7d5920de24cbed3cc6646980b3ca926e4dc27fa5
A138634 <dsmith9@agl.com.au>
2019-11-18 16:33:01 +1100

    First complete working version

--- DotXML/DotXML.cs
+++ DotXML/DotXML.cs
@@ -7,81 +7,102 @@ namespace DotXML
 {
     /// <summary>
     /// Wrapper for XmlDocument class to support dot-notation reference
-    ///     use Parse(XmlDocument) to build an object tree
-    ///         <tags> are converted to name:value pairs
-    ///         unless there are multiple instances within a container 
-    ///         in which case they are converted to an array of name:value pairs
+    ///     use 
+    ///         (DotXML)doc.LoadXML(text)
+    ///     to build an object tree
+    ///         * <tags> are converted to name:value pairs (XDict)
+    ///             unless there are multiple instances within a container 
+    ///             in which case they are converted to an array of name:value pairs (XArray)
     ///         
-    ///         TODO: elementary values are decoded a long, double, bool, string or null
+    ///         *attributes are added as a node called 'attr'
     ///         
-    ///         TODO: attributes are added as a node called 'attr'
+    ///         TODO: elementary values are decoded as long, double, bool, string or null
     ///         
-    ///     use Item("node.array[index].item") to access objects
-    ///         the Item returned can be a node, array or element
+    ///     use Item("<node>.<array>[<index>].<item>") to access the named item
+    ///         the Item returned can be a XNode, XArray or element
+    ///     use Item("<node>.attr.<name>" to access the named attribute
     ///         
     /// </summary>
     public class DotXML : XmlDocument
     {
-        public XNode body = null;
+        public XNode body;
         public override void LoadXml(string xml)
         {
             base.LoadXml(xml);
             body = new XNode { };
-            Parse(ref body, ChildNodes);
+            Parse(ref body, this);
         }
 
-        internal static dynamic Parse(ref XNode root, XmlNodeList Content)
+        #region Parse XML document
+        // convert elementary value to primary Type
+        internal static dynamic Element( string Value)
         {
-            // construct an object 'tree' of the XML document elements
-            foreach(XmlNode Node in Content)
-            {
-                string name = Node.Name;
-                dynamic Value = null;
-                XNode Attrs = null;
+            dynamic result = Value;
+            if( long.TryParse(Value, out long lRresult) )
+                result = lRresult;
+            else if (double.TryParse(Value, out double dResult))
+                result = dResult;
+            else if (Value.ToLower() == "true")
+                result = true;
+            else if (Value.ToLower() == "false")
+                result = false;
+            return result;
+        }
 
-                if (Node.Attributes.Count > 0)
+        // construct an object 'tree' of the XML document elements
+        internal static dynamic Parse(ref XNode root, XmlNode Node)
+        {
+            // Add attributes list if present
+            if( Node.Attributes!= null && Node.Attributes.Count > 0 )
+            {
+                XNode Attrs = new XNode { };
+                foreach (XmlAttribute Attr in Node.Attributes)
                 {
-                    Attrs = new XNode { };
-                    foreach (XmlAttribute Attr in Node.Attributes)
-                    {
-                        Attrs[Attr.Name] = Attr.Value;
-                    }
+                    Attrs[Attr.Name] = Attr.Value;
                 }
+                root["attr"] = Attrs;
+            }
+
+            // add children: branches or elements
+            foreach (XmlNode Child in Node.ChildNodes )
+            {
+                string name = Child.Name;
+                dynamic Value = null;
 
                 // extract the node Value
-                if ( Node.ChildNodes.Count == 0)
-                    Value = Node.Value;
-                else if(Node.ChildNodes.Count == 1 && Node.FirstChild.Name == "#text")
-                    Value = Node.FirstChild.Value;
+                if ( Child.ChildNodes.Count == 0)
+                    root[name] = Element(Child.Value);
+                else if(Child.ChildNodes.Count == 1 && Child.FirstChild.Name == "#text")
+                    root[name] = Element(Child.FirstChild.Value);
                 else
                 {
                     XNode branch = new XNode { };
-                    Value = Parse(ref branch, Node.ChildNodes);
-                    if (Attrs != null)
+                    Value = Parse(ref branch, Child);
+
+                    // add the item to the tree
+                    if (!root.ContainsKey(name))
                     {
-                        Value["attr"] = Attrs;
+                        root[name] = Value;
+                    }
+                    // if multiple items of this name convert then to XList item
+                    else if (root[name].GetType().Name != "XList")
+                    {
+                        dynamic First = root[name];
+                        root.Remove(name);
+                        root[name] = new XList { First, Value };
+                    }
+                    else
+                    {
+                        root[name].Add(Value);
                     }
                 }
 
-                // add the item to the tree
-                if( ! root.ContainsKey(name))
-                {
-                    root[name] = Value;
-                }
-                // if multiple items of this name convert then to XList item
-                else if( root[name].GetType().Name != "XList")
-                {
-                    dynamic First = root[name];
-                    root.Remove(name);
-                    root[name] = new XList { First, Value };
-                }
-                else
-                {
-                    root[name].Add(Value);
-                }
             }
             return root;
         }
+        #endregion
+
+        #region dot-notation access methods
         public class XList : List<dynamic>
         {
             public dynamic Item(int index) => (index >= 0 && index < Count) ? this[index] : null;
@@ -96,8 +117,19 @@ namespace DotXML
                 if (key.IndexOf('.') < 0)
                 {
                     if (key.IndexOf('[') < 0)
+                    {
                         // fetch the referenced element
-                        return (this.ContainsKey(key) ? this[key] : null);
+                        dynamic Element = null;
+                        if( this.ContainsKey(key) ) Element = this[key];
+                        try
+                        {
+                            return Element["Value"];
+                        }
+                        catch
+                        {
+                            return Element;
+                        }
+                    }
                     else
                     {
                         string node = key.Substring(0, key.IndexOf('['));
@@ -120,5 +152,7 @@ namespace DotXML
                 return (int.TryParse(content, out int index)) ? index : -1;
             }
         }
+        #endregion
+
     }
 }
