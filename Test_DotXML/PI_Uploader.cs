4ed367c81d7a6df263b25843c05b121365116747
DelS-AGL <45987691+DelS-AGL@users.noreply.github.com>
2019-12-24 10:59:53 +1100

    Update PI_Uploader.cs

--- Test_DotXML/PI_Uploader.cs
+++ Test_DotXML/PI_Uploader.cs
@@ -24,6 +24,8 @@ namespace EventHubReceiver
         ///
 
         internal static DateTime unix0time = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        internal static int hourMark = -1;
+
         internal static DateTime ConvertTime(string time, string style)
         {
             switch (style) {
@@ -78,6 +80,11 @@ namespace EventHubReceiver
             string time_style = msg_profile.Item("message.time_style","unix");
             var time = msg.Item("time");
             DateTime sampleTime = ConvertTime( $"{time}", time_style );
+            if (sampleTime.Hour != hourMark)
+            {
+                hourMark = sampleTime.Hour;
+                Console.WriteLine($"{sampleTime} : {source}.{device_type}.{device}");
+            }
             Dictionary<string,DotXML.XNode> pt_info = IoT_Profile.Point_Info(msg_profile);
             if(pt_info == null)
             {
@@ -138,176 +145,5 @@ namespace EventHubReceiver
             }
 
         } // end PI_Upload()
-
-        //internal static void UploadPoint(dynamic point, dynamic Value, string tag, string pointsource, DateTime sampleTime)
-        //{
-        //    if (Value != null)
-        //    {
-
-        //        PIPoint p = null;
-        //        try
-        //        {
-        //            p = PIPoint.FindPIPoint(server, tag);
-        //        }
-        //        catch (PIException e)
-        //        {
-        //            try
-        //            {
-        //                p = CreatePIPoint(server, pointsource, tag, point);
-        //            }
-        //            catch
-        //            {
-        //                Console.WriteLine($"Find/Create PI Point exception\n{e.Message}");
-        //                return;
-        //            }
-        //        }
-
-        //        AFValue value;
-        //        try
-        //        {
-        //            switch (p.PointType)
-        //            {
-        //                case PIPointType.Float16:
-        //                case PIPointType.Float32:
-        //                case PIPointType.Float64:
-        //                    value = new AFValue((double)Value);
-        //                    break;
-        //                case PIPointType.Int16:
-        //                case PIPointType.Int32:
-        //                    value = new AFValue((int)Value);
-        //                    break;
-        //                case PIPointType.Digital:
-        //                case PIPointType.String:
-        //                default:
-        //                    value = new AFValue(Value.ToString());
-        //                    break;
-        //            }
-        //            value.Timestamp = (AFTime)sampleTime;
-        //        }
-        //        catch (PIException e)
-        //        {
-        //            Console.WriteLine($"Conversion exception\n{e.Message}");
-        //            return;
-        //        }
-
-        //        try
-        //        {
-        //            Console.WriteLine($"Tag: {tag:S32}, Time: {sampleTime}, Value: {Value}");
-        //            p.UpdateValue(value, updateOption, bufferOption);
-        //        }
-        //        catch (PIException e)
-        //        {
-        //            Console.WriteLine($"PI Point Update exception\n{e.Message}");
-        //            return;
-        //        }
-        //        finally { }
-        //    }
-        //}
-
-        internal static PIPoint CreatePIPoint(PIServer server, string pointsource, string tag, DotXML.XNode pointSpec)
-        {
-            Dictionary<string, Object> attributes = new Dictionary<string, object> { };
-            string pointtype = pointSpec.Item("type") ?? "Float32";
-            attributes.Add("pointtype", pointtype);
-            attributes.Add("engunits", pointSpec.Item("engunits") ?? "");
-            attributes.Add("pointsource", pointsource ?? "");
-            attributes.Add("descriptor", tag.Replace('.',' ').Replace('_',' '));
-            attributes.Add("excdev", 0);
-            attributes.Add("compdev", 0);
-            attributes.Add("shutdown", 0);
-            attributes.Add("location5", 1);
-            if( pointtype.ToLower() == "digital")
-                attributes.Add("digitalset", pointSpec.Item("digitalset") ?? "");
-            try
-            {
-                PIPoint p = server.CreatePIPoint(tag, attributes);
-                Console.WriteLine($"Point {p.Name} created");
-                return p;
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine($"PIPoint Creation failed\n{e.Message}");
-                return null;
-            }
-        }
-
-        internal class PI_Update
-        {
-            internal readonly PIPoint Point;
-            internal readonly AFValues Values;
-
-            internal PI_Update(PIPoint p, AFValues v) { Point = p; Values = v; }
-        }
-
-        internal static ConcurrentQueue<PI_Update> OutQueue = null;
-        //static AFUpdateOption updateOption;
-        //static AFBufferOption bufferOption;
-
-        //private static Thread thUpload;
-        //private static bool Start_Uploader(PIServer server)
-        //{
-        //    // don't start twice
-        //    if (OutQueue != null)
-        //        return false;
-
-        //    // create the transaction queue
-        //    OutQueue = new ConcurrentQueue<PI_Update> { };
-
-        //    // start the Uploader thread
-        //    thUpload = new Thread(() => Uploader(server));
-        //    thUpload.Start();
-        //    while (!thUpload.IsAlive) Thread.Sleep(100);
-
-        //    // define OSI-PI update parameters
-        //    bool compress = (settings.Item("compression").ToLower() == "true");
-        //    updateOption = compress ? AFUpdateOption.Insert : AFUpdateOption.InsertNoCompression;
-        //    bufferOption = AFBufferOption.BufferIfPossible;
-
-        //    return true;
-        //}
-
-        //private static bool Stop_Uploader()
-        //{
-        //    if (thUpload.IsAlive)
-        //    {
-        //        // assuming listeners have shut down, wait for queue to empty
-        //        while (OutQueue.Count > 0) Thread.Sleep(330);
-
-        //        // wait for the worker thread to finish
-        //        while (thUpload.IsAlive) Thread.Sleep(330); 
-
-        //        Console.WriteLine("PI Uploader Stopped");
-        //    }
-        //    return true;
-        //}
-
-        //// This function is started in its own thread and runs as long as it is 'enabled'
-        //private static void Uploader(PIServer server)
-        //{
-        //    int MaxQueue = 0;
-        //    int Count = 0;
-        //    while (enabled || OutQueue.Count > 0)
-        //    {
-        //        if (OutQueue.TryDequeue(out PI_Update next))
-        //        {
-        //            MaxQueue = Math.Max(OutQueue.Count, MaxQueue);
-        //            Count++;
-        //            PIPoint p = next.Point;
-        //            AFValues v = next.Values;
-        //            AFErrors<AFValue> result = p.UpdateValues(v, updateOption, bufferOption);
-        //            Thread.Sleep(10);
-
-        //            if (result != null && result.HasErrors)
-        //                foreach (var item in result.Errors)
-        //                    Console.WriteLine($"  AFValue '{item.Key}': {item.Value}");
-        //        }
-        //        else
-        //        {
-        //            Thread.Sleep(100);
-        //        }
-        //        Thread.Sleep(1);
-        //    }
-        //    Console.WriteLine($"Uploader exiting. Transactions: {Count:D6}, Max Queue: {MaxQueue:D4}");
-        //}
     }
 }
